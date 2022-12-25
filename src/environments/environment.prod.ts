3f51f0fa79185e78185f7a6f1303e19a062e3e23
vipulaneranjana <vipulana17@gmail.com>
2022-12-25 21:52:04 +0530

    deploying app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+
+  headers: {
+		'X-RapidAPI-Key': 'b15905d8a4msh8f1cc0a31f0f9b2p1344d0jsnbb6eb3bb1871',
+		'X-RapidAPI-Host': 'bestweather.p.rapidapi.com'
+	},
+  url: 'https://bestweather.p.rapidapi.com/weather/',
+
+  // for 2nd free api
+  url2 :  'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
+  headers2: {
+		'X-RapidAPI-Key': 'b15905d8a4msh8f1cc0a31f0f9b2p1344d0jsnbb6eb3bb1871',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
 };
